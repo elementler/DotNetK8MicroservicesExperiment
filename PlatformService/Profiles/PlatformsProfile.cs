fda5c6fcbbb3739c160ea8b0e7a89be91d82333e
Xavier <elementler@hotmail.com>
2021-12-07 18:35:52 -0800

    add source code

---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformsProfile : Profile
+    {
+        public PlatformsProfile()
+        {
+            // Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+            CreateMap<PlatformReadDto, PlatformPublishedDto>();
+            CreateMap<Platform, GrpcPlatformModel>()
+                .ForMember(dest => dest.PaltformId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Publisher, opt => opt.MapFrom(src => src.Publisher));
+        }
+    }
+}
